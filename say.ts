ec77877a939ba15b9d073ac5d1910dad1c22c419
bruno palma <bruno.palma@xempus.com>
2024-05-06 02:00:51 +0200

    love

--- say.ts
+++ say.ts
@@ -1,9 +1,7 @@
 import * as cowsay from 'cowsay'
 import * as fs from 'fs'
 import * as path from 'path'
-import * as wrap from 'word-wrap'
-
-import lolcat from 'isomorphic-lolcat'
+import wrap from 'word-wrap'
 
 // Read the quotes from the JSON file
 const quotes: string[] = JSON.parse(fs.readFileSync(path.join(__dirname, 'tech_quotes.json'), 'utf-8'))
@@ -12,7 +10,14 @@ const quotes: string[] = JSON.parse(fs.readFileSync(path.join(__dirname, 'tech_q
 const randomQuote = quotes[Math.floor(Math.random() * quotes.length)]
 
 // Wrap the text at 120 characters
-const wrappedQuote = wrap(randomQuote, { width: 40 })
+const wrappedQuote = wrap(randomQuote, {
+  width: 40,
+  indent: '',
+  trim: true,
+  cut: false,
+  newline: '\n',
+  escape: (str: string) => str,
+})
 
 // Generate the cowsay message
 const message = cowsay.say({ text: wrappedQuote })
---
+++ tweet.ts
@@ -0,0 +1,47 @@
+import { TwitterApi } from 'twitter-api-v2'
+
+// Twitter API credentials
+const client = new TwitterApi({
+  appKey: '7JEsrkvDVxDPLC49PiPvgKnF0',
+  appSecret: 'ROBhag2UN0lx99ezY1gbMl9ezkiIMftXoGmxOGE5rTDY6EjtuS',
+  accessToken: '1786892620821659648-D73mNAhh70wHFGCKyNJ7RDmRWrWEFu',
+  accessSecret: '1TGZdUxFR2IjOmAkjtQwuLcOmq9Z9RFhcGPgr0IOjmGYn',
+})
+
+// This is the client that can interact with the Twitter API
+const rwClient = client.readWrite
+
+// Function to upload an image and return the media ID
+async function uploadMedia(filePath: string): Promise<string> {
+  try {
+    const mediaData = await rwClient.v1.uploadMedia(filePath, { mimeType: 'image/jpeg' })
+    console.log('🚀 ~ uploadMedia ~ mediaData:', mediaData)
+    return mediaData
+  } catch (error) {
+    console.error('Could not upload media:', error)
+    throw error // Rethrow to handle in the calling function
+  }
+}
+
+// Function to post a tweet
+async function postTweet(message: string): Promise<void> {
+  try {
+    const tweet = await rwClient.v2.tweet(message)
+    console.log('Tweeted:', tweet.data)
+  } catch (error) {
+    console.error('Could not post tweet:', error)
+  }
+}
+
+// Function to post a tweet with an image
+async function postTweetWithImage(message: string, imagePath: string): Promise<void> {
+  try {
+    const mediaId = await uploadMedia(imagePath)
+    const tweet = await rwClient.v2.tweet(message, { media: { media_ids: [mediaId] } })
+    console.log('Tweeted:', tweet.data)
+  } catch (error) {
+    console.error('Could not post tweet:', error)
+  }
+}
+
+postTweetWithImage('Hello world!', 'quote.png')
